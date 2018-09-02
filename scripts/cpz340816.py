22df86c33dff38e21536b05e3e2764bac0e8da73
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-09-02 07:03:07 +0000

    debug: callback method :seedling:

--- scripts/cpz340816.py
+++ scripts/cpz340816.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import threading
 import pyinterface
 
 import rospy
@@ -24,7 +25,7 @@ class cpz340816_controller(object):
         self.topic_list = [rospy.get_param('~topic{}'.format(i+1)) for i in range(ch_number)]
         self.pub_list = [rospy.Publisher(topic, Float64, queue_size=1)
                          for topic in self.topic_list]
-        self.sub_list = [rospy.Subscriber(topic+'_cmd', Float64, self.output_voltage, callback_args=ch)
+        self.sub_list = [rospy.Subscriber(topic+'_cmd', Float64, self.set_param, callback_args=ch)
                          for ch, topic in enumerate(self.topic_list, start=1)]
 
     def set_param(self, req, ch):
@@ -40,13 +41,18 @@ class cpz340816_controller(object):
                 time.sleep(0.01)
                 continue
 
-            da.output_voltage(self.ch, self.voltage)
-            self.pub_list[self.ch-1].Publish(self.voltage)
+            self.da.output_voltage(self.ch, self.voltage)
+            self.pub_list[self.ch-1].publish(self.voltage)
             self.flag = 1
             continue
 
-        
+    def start_thread_ROS(self):
+        th = threading.Thread(target=self.output_voltage)
+        th.setDaemon(True)
+        th.start()
+
 if __name__ == '__main__':
     rospy.init_node(node_name)
-    cpz340816_controller()
+    ctrl = cpz340816_controller()
+    ctrl.start_thread_ROS()
     rospy.spin()
