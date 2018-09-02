e66e66392d77c9bf29aaaec50811f6fdf3c6685d
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-09-02 07:02:22 +0000

    debug: select ch in main :thumbsup:

--- scripts/cpz3177.py
+++ scripts/cpz3177.py
@@ -10,7 +10,12 @@ from std_msgs.msg import Float64
 
 
 def str2list(param):
-    return param.strip('[').strip(']').split(',')
+    _list = [_.strip(' ') for _ in list(param.strip('[').strip(']').split(','))]
+    ret = _list
+    ret[1] = _list[1][1:-1]
+    ret[2] = int(_list[2])
+
+    return list(ret)
 
 
 if __name__ == '__main__':
@@ -21,7 +26,7 @@ if __name__ == '__main__':
     onoff_index = 2
 
     rospy.init_node(node_name)
-    
+
     rsw_id = rospy.get_param('~rsw_id')
     topic_list = [str2list(rospy.get_param('~topic{}'.format(i+1)))
                        for i in range(ch_number)]
@@ -42,7 +47,7 @@ while not rospy.is_shutdown():
         for pub, msg, param in zip(pub_list, msg_list, param_list):
             topic = param[0]
             inputrange = param[1]
-            ch = topic[-1]
+            ch = int(topic.replace('cpz3177_ch', ''))
             
             ret = ad.input_voltage(ch, inputrange)
             
