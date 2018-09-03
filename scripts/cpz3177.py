fb152159cfe8c33b64cf5296041c311443344e5d
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-09-03 19:06:51 +0000

    info: gradeup & tested simply :sleeping:

--- scripts/cpz3177.py
+++ scripts/cpz3177.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import numpy
 import pyinterface
 
 import rospy
@@ -9,32 +10,37 @@ import std_msgs
 from std_msgs.msg import Float64
 
 
-def str2list(param):
-    _list = [_.strip(' ') for _ in list(param.strip('[').strip(']').split(','))]
-    ret = _list
-    ret[1] = _list[1][1:-1]
-    ret[2] = int(_list[2])
+def select_singlediff():
+    parse = lambda diffch_list: diffch_list.strip('[').strip(']').split(',')
+    diffch_list = list(map(int, parse(rospy.get_param('~diffch_list'))))
+    ch_array = numpy.arange(1, ch_number['single'] + 1, 1).reshape(8, 8)
+    get_prohibit_singlech = lambda ch: [ch_array[int((ch - 1) / 8 * 2)][ch % 8 -1],
+                                        ch_array[int((ch - 1) / 8 * 2 + 1)][ch % 8 - 1]]
+    remove_list = []
+    [remove_list.extend(get_prohibit_singlech(ch)) for ch in diffch_list]
+    singlech_list = [_ for _ in range(1, ch_number['single'] + 1)]
+    [singlech_list.remove(_) for _ in remove_list]
 
-    return list(ret)
+    ret = []
+    [ret.append([ch, 'single']) for ch in singlech_list]
+    [ret.append([ch, 'diff']) for ch in diffch_list]
+
+    return ret
 
 
 if __name__ == '__main__':
-    ch_number = 64
-    node_name = 'cpz3177'    
-    topic_name_index = 0
-    inputmode_index = 1
-    onoff_index = 2
+    ch_number = {'single': 64, 'diff': 32}        
+    node_name = 'cpz3177'
 
     rospy.init_node(node_name)
-
-    rsw_id = rospy.get_param('~rsw_id')
-    topic_list = [str2list(rospy.get_param('~topic{}'.format(i+1)))
-                       for i in range(ch_number)]
-    pub_list = [rospy.Publisher(topic[topic_name_index], Float64, queue_size=1) \
-                     for topic in topic_list if int(topic[onoff_index]) == 1]
-    param_list = [[topic[topic_name_index], topic[inputmode_index]]
-                       for topic in topic_list if int(topic[onoff_index]) ==1]
-    msg_list = [Float64() for i in range(len(param_list))]
+    rate = rospy.get_param('~rate')
+    rsw_id = rospy.get_param('~rsw_id')        
+    rate = rospy.get_param('~rate')
+    rsw_id = rospy.get_param('~rsw_id')        
+    
+    topic_list = select_singlediff()
+    pub_list = [rospy.Publisher('{0}_AIN_{1}{2}'.format(node_name, mode, ch), Float64, queue_size=1)
+                       for ch, mode in topic_list]
 
     try:
         ad = pyinterface.open(3177, rsw_id)
@@ -44,13 +50,14 @@ if __name__ == '__main__':
 
 while not rospy.is_shutdown():
 
-        for pub, msg, param in zip(pub_list, msg_list, param_list):
-            topic = param[0]
-            inputrange = param[1]
-            ch = int(topic.replace('cpz3177_ch', ''))
+    for param, pub in zip(topic_list, pub_list):
+        ch = param[0]
+        mode = param[1]
+        msg = Float64()    
+
+        ret = ad.input_voltage(ch, mode)
             
-            ret = ad.input_voltage(ch, inputrange)
-            
-            msg.data = ret
-            pub.publish(msg)
-        continue
+        msg.data = ret
+        pub.publish(msg)
+    continue
+
