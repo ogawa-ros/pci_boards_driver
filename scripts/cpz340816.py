400ee013b98f2d4ddab01a27079001c4b6473b24
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-09-04 07:09:44 +0000

    info: add param buffer, lock and tested :fire:

--- scripts/cpz340816.py
+++ scripts/cpz340816.py
@@ -6,45 +6,72 @@ import threading
 import pyinterface
 
 import rospy
-import std_msgs
+from std_msgs.msg import Int32
 from std_msgs.msg import Float64
 
 
 ch_number = 16
-node_name = 'cpz340816'
 
 
 class cpz340816_controller(object):
 
     def __init__(self):
+        self.rate = rospy.get_param('~rate')
         self.rsw_id = rospy.get_param('~rsw_id')
+        self.node_name = rospy.get_param('~node_name')
         self.flag = 1
-        self.ch = 1
-        self.voltage = 0.
+        self.lock = 0
+        self.param_buff = []        
+
         self.da = pyinterface.open(3408, self.rsw_id)
-        self.topic_list = [rospy.get_param('~topic{}'.format(i+1)) for i in range(ch_number)]
+        
+        self.topic_list = [('{0}_rsw{1}_{2}'.format(self.node_name, self.rsw_id, _))
+                           for _ in range(1, ch_number + 1)]
         self.pub_list = [rospy.Publisher(topic, Float64, queue_size=1)
                          for topic in self.topic_list]
         self.sub_list = [rospy.Subscriber(topic+'_cmd', Float64, self.set_param, callback_args=ch)
                          for ch, topic in enumerate(self.topic_list, start=1)]
-
+        self.sub_lock = rospy.Subscriber('{0}_rsw{1}_lock'
+                                         .format(self.node_name, self.rsw_id), Int32, self.set_lock)
+        
+    def set_lock(self, req):
+        self.lock = req.data
+        pass
+        
     def set_param(self, req, ch):
-        self.ch = ch
-        self.voltage = req.data
+        self.param_buff.append({'{}'.format(ch): req.data})
         self.flag = 0
         pass
-
+            
     def output_voltage(self):
         while not rospy.is_shutdown():
 
             if self.flag == 1:
-                time.sleep(0.01)
+                time.sleep(self.rate)
                 continue
-
-            self.da.output_voltage(self.ch, self.voltage)
-            self.pub_list[self.ch-1].publish(self.voltage)
-            self.flag = 1
-            continue
+            
+            param_buff = self.param_buff.copy()
+            self.param_buff = []
+            
+            for param in param_buff:
+                ch = int(list(param.keys())[0])
+                voltage = list(param.values())[0]
+                
+                if self.lock == 0:
+                    self.da.output_voltage(ch, voltage)
+                    
+                elif self.lock == 1:
+                    while not(self.lock == 0):
+                        print('[Worning] Lock is ON, contorol {} is prohibited.'
+                              .format(self.node_name))
+                        time.sleep(1)
+                    self.param_buff, param_buff = [], []
+                    print('[Info] Lock is OFF, and ouput voltage data is deleted.')
+                    break
+                
+                self.pub_list[ch-1].publish(voltage)
+                self.flag = 1
+                continue
 
     def start_thread_ROS(self):
         th = threading.Thread(target=self.output_voltage)
@@ -52,7 +79,7 @@ class cpz340816_controller(object):
         th.start()
 
 if __name__ == '__main__':
-    rospy.init_node(node_name)
+    rospy.init_node('cpz340816')
     ctrl = cpz340816_controller()
     ctrl.start_thread_ROS()
     rospy.spin()
