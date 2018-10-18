0e275d0f68132e3a8c74eed91bee77ac19ef2295
k.noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-10-18 14:37:25 +0900

    improve cpz2724m

--- scripts/cpz2724m.py
+++ scripts/cpz2724m.py
@@ -11,11 +11,18 @@ import std_msgs.msg
 import pyinterface
 
 
+
 class CPZ2724(object):
 
+    ch_list = ["01","02","03","04","05","06","07","08","09","10",
+                "11","12","13","14","15","16","17","18","19","20",
+                "21","22","23","24","25","26","27","28","29","30",
+                "31","32"]
+    ch_list_byte = []
+    ch_list_word = ["1_16, 17_32"]
+    ch_list_dword = []
+    
     def __init__(self):
-        self.ch_list_di = rospy.get_param("~ch_list_di")
-        self.ch_list_do = rospy.get_param("~ch_list_do")
         self.rsw_id = rospy.get_param("~rsw_id")
 
         self.pub = [rospy.Publisher(
@@ -23,15 +30,39 @@ class CPZ2724(object):
                         data_class = std_msgs.msg.Bool,
                         latch = True,
                         queue_size = 1
-                    ) for ch in self.ch_list_di]
+                    ) for ch in self.ch_list]
 
         self.sub = [rospy.Subscriber(
                         name = "cpz2724_rsw{0}/do{1}".format(self.rsw_id, ch),
                         data_class = std_msgs.msg.Bool,
-                        callback = self.output_function,
+                        callback = self.output_point,
                         callback_args = ch,
                         queue_size = 1
-                    ) for ch in self.ch_list_do]
+                    ) for ch in self.ch_list]
+
+        self.sub_byte = [rospy.Subscriber(
+                        name = "cpz2724_rsw{0}/do{1}".format(self.rsw_id, ch),
+                        data_class = std_msgs.msg.Bool,
+                        callback = self.output_byte,
+                        callback_args = ch,
+                        queue_size = 1
+                    ) for ch in self.ch_list_byte]
+
+        self.sub_word = [rospy.Subscriber(
+                        name = "cpz2724_rsw{0}/do{1}".format(self.rsw_id, ch),
+                        data_class = std_msgs.msg.Bool,
+                        callback = self.output_word,
+                        callback_args = ch,
+                        queue_size = 1
+                    ) for ch in self.ch_list_word]
+
+        self.sub_dword = [rospy.Subscriber(
+                        name = "cpz2724_rsw{0}/do{1}".format(self.rsw_id, ch),
+                        data_class = std_msgs.msg.Bool,
+                        callback = self.output_dword,
+                        callback_args = ch,
+                        queue_size = 1
+                    ) for ch in self.ch_list_dword]
 
         try:
             self.dio = pyinterface.open(2724, self.rsw_id)
@@ -41,16 +72,36 @@ class CPZ2724(object):
         
         pass
 
-    def output_function(self, req, ch):
-        self.dio.output_point(req.data ,int(ch))
+    def output_point(self, req, ch):
+        self.dio.output_point([req.data] ,int(ch))
         return
 
+    def output_byte(self, req, ch):
+        self.dio.output_byte("OUT{}".format(ch), req.data)
+        return
+
+    def output_word(self, req, ch):
+        self.dio.output_word("OUT{}".format(ch), req.data)
+        return
+
+    def output_dword(self, req, ch):
+        self.dio.output_dword("OUT{}".format(ch), req.data)
+        return
+
+
     def pub_function(self):
+        di_list = self.dio.input_dword()
+        for ch, pub in zip(self.ch_list, self.pub):
+            pub.publish(di_list[int(ch)-1])
+
         while not rospy.is_shutdown():
-            for ch, pub in zip(self.ch_list_di, self.pub):
-                ret = self.dio.input_point(int(ch), 1)
-                pub.publish(ret)
-            
+            ret = self.dio.input_dword() # ch1~32
+            for ch, pub in zip(self.ch_list, self.pub):
+                if ret[int(ch)-1] != di_list[int(ch)-1]:
+                    pub.publish(ret[int(ch)-1])
+                    di_list[int(ch)-1] = ret[int(ch)-1]
+                else: pass
+
             time.sleep(0.001)
             continue
         
