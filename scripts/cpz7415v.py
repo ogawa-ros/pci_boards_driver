44f0baec155ea4e3ab5af80bb7e9a370da6d8710
Shota Ueda <ueda@s7u27m.local>
2018-10-24 07:27:42 +0900

    test

--- scripts/cpz7415v.py
+++ scripts/cpz7415v.py
@@ -139,7 +139,7 @@ class cpz7415v_controller(object):
                 continue
             ###=== set speed ===###
             self.mot.change_speed(axis=self.axis, spd=self.speed_cmd_li)
-            self.target_spd = self.speed_cmd_li[0]
+            self.target_speed = self.speed_cmd_li[0]
             self.speed_cmd_li = []
             self.speed_cmd_flag = False
             continue
