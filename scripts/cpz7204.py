fc1f919486acb48ad4e7d2add59d28111db3183f
k.noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-11-09 15:46:27 +0900

    create-cpz7204

--- scripts/cpz7204.py
+++ scripts/cpz7204.py
@@ -16,39 +16,66 @@ class CPZ7204(object):
     def __init__(self):
         self.rsw_id = rospy.get_param("~rsw_id")
 
-        self.pub = rospy.Publisher(
-                        name = "/cpz7204_rsw{0}/di{1}".format(self.rsw_id),
+        self.pub_busy = rospy.Publisher(
+                        name = "/cpz7204_rsw{0}/busy".format(self.rsw_id),
+                        data_class = std_msgs.msg.Bool,
+                        latch = True,
+                        queue_size = 1,
+                    )
+
+        self.pub_pEL = rospy.Publisher(
+                        name = "/cpz7204_rsw{0}/p_EL".format(self.rsw_id),
+                        data_class = std_msgs.msg.Bool,
+                        latch = True,
+                        queue_size = 1,
+                    )
+
+        self.pub_mEL = rospy.Publisher(
+                        name = "/cpz7204_rsw{0}/m_EL".format(self.rsw_id),
                         data_class = std_msgs.msg.Bool,
                         latch = True,
                         queue_size = 1,
                     )
 
         self.sub = rospy.Subscriber(
-                        name = "/cpz7204_rsw{0}/do{1}".format(self.rsw_id),
+                        name = "/cpz7204_rsw{0}/step".format(self.rsw_id),
                         data_class = std_msgs.msg.Bool,
-                        callback = self.output_function,
+                        callback = self.move_function,
                         queue_size = 1, 
                     )
 
         try:
-            self.dio = pyinterface.open(7204, self.rsw_id)
+            self.mot = pyinterface.open(7204, self.rsw_id)
+            self.mot.initialize()
+            self.mot.set_limit_config('LOGIC', '+EL -EL', axis=1)
         except OSError as e:
             rospy.logerr(e, name, self.rsw_id)
             sys.exit()
         
         pass
 
-    def output_function(self, req, ch):
-        self.dio.output_point(req.data ,int(ch))
+    def move_function(self, req):
+        if req.data==True: #NASCO
+            self.mot.set_motion(mode="JOG", step = -1)
+        else: #SMART
+            self.mot.set_motion(mode="JOG", step = 1)
+        self.mot.start_motion(mode="JOG")
         return
 
     def pub_function(self):
+        status_last = None
         while not rospy.is_shutdown():
-            for ch, pub in zip(self.ch_list_di, self.pub):
-                ret = self.dio.input_point(int(ch), 1)
-                pub.publish(ret)
+            status = self.mot.get_status()
+
+            if status != status_last:
+                self.pub_busy.publish(status["busy"])
+                self.pub_pEL.publish(status["limit"]["+EL"])
+                self.pub_mEL.publish(status["limit"]["-EL"])
+
+                status_last = status
+            else: pass
             
-            time.sleep(0.001)
+            time.sleep(0.01)
             continue
         
         return
